3ed5c80aefd734fe3097f0a4652d75ecc82ef527
majorbobkov <pdobrinskiy@yahoo.com>
2021-09-14 12:44:13 +0300

    pyats rest example

---
+++ test_apic.py
@@ -0,0 +1,15 @@
+from  genie.testbed import load
+from pprint import pprint
+from pyats.topology import loader
+from genie.utils import Dq
+
+
+tb = loader.load('apic.yaml')
+apic = tb.devices['apic1']
+apic.connect()
+output = apic.get('api/node/class/faultInst.json')
+pprint (output)
+
+dns = Dq(output).get_values('dn')
+
+pprint (dns)
